f9af1c058e1ab764165c4a9558bf0ed42a80c03f
soporteNahuelSalinas <soporte.anyway02@gmail.com>
2025-01-16 15:24:44 -0300

    Update fullscreen.js

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,10 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const elem = document.documentElement; // Selecciona todo el documento para fullscreen
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-});
+const fullscreenButton = document.getElementById('fullscreen-btn');
+
+    fullscreenButton.addEventListener('click', () => {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen()
+          .then(() => {
+            fullscreenButton.textContent = 'Salir';
+          })
+          .catch((err) => {
+            console.error(`Error al intentar entrar en modo pantalla completa: ${err.message}`);
+          });
+      } else {
+        document.exitFullscreen()
+          .then(() => {
+            fullscreenButton.textContent = 'Ampliar';
+          })
+          .catch((err) => {
+            console.error(`Error al intentar salir del modo pantalla completa: ${err.message}`);
+          });
+      }
+    });
